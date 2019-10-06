a6614f8262b38a9cbc2e96972c14e9a050ee5cdb
Dexter Cai <caiwenzhe2021@gmail.com>
2019-10-06 20:45:50 +0800

    readme

--- src/Leaflet.SpriteMarker.js
+++ src/Leaflet.SpriteMarker.js
@@ -1,4 +1,4 @@
-L.Marker.SpriteMarker = L.Marker.extend({
+L.Marker.SpriteMarkerBuilder = L.Marker.extend({
     initialize: function(position, icon) { // (LatLng, Object)
 		var divIcon = L.divIcon({
 			html: '<div style="background:url(' + icon.url + ') no-repeat; background-position:-' + icon.origin.x + 'px -' + icon.origin.y + 'px ;width: ' + icon.size.width + 'px;height: ' + icon.size.height + 'px;margin-left:-' + icon.anchor.x + 'px;margin-top:-' + icon.anchor.y + 'px;"></div>'
@@ -11,5 +11,5 @@ L.Marker.SpriteMarker = L.Marker.extend({
 });
 
 L.Marker.SpriteMarker = function(position, icon) {
-    return new L.Marker.SpriteMarker(position, icon);
+    return new L.Marker.SpriteMarkerBuilder(position, icon);
 };
